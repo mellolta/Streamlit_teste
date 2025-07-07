5dd3c7e7a7ba4f4616b24a8291d405876b3a9900
Luis Tomas Mello <luistomas.mello@gmail.com>
2025-07-06 21:10:31 -0300

    modificação

--- Api.py
+++ Api.py
@@ -2,36 +2,30 @@
 
 import streamlit as st
 import pandas as pd
-import tkinter as tk
-from  tkinter import filedialog
 
 """Teste de aplicação"""
 
 #< ------------------------------------------------------------------------------------------------------------------------------
-def seleciona_arquivo():
-    """ Abre uma janela de navegação que permite selecionar um único arquivo.\n
-        Retorna o path do arquivo.\n
-        - Esse procedimento evita que a janela de navegação fique atrás da janela de trabalho do VSCode.\n
-    """
+def seleciona_arquivo_streamlit():
+    # st.title("Meu Visualizador de Excel")
 
-    root = tk.Tk()
-    root.withdraw()  #- Oculta a janela principal (evita que a janela principal seja exibida por um breve momento)
+    # Usa st.file_uploader para permitir que o usuário faça upload de um arquivo
+    # uploaded_file = st.file_uploader("Escolha um arquivo Excel (.xls ou .xlsx)", type=["xls", "xlsx"])
+    uploaded_file = st.file_uploader("Escolha um arquivo")
 
-    #- Mostra o diálogo de abertura de arquivo em primeiro plano
-    root.lift()                         #- traz a janela principal para o primeiro plano
-    root.attributes('-topmost', True)   #- tornar a janela principal a janela superior
-    root.focus_force()
-
-    #- Abre a janela de navegação e salva na variável
-    arquivo = filedialog.askopenfilename()
-
-    #- Restaura o comportamento padrão
-    root.attributes('-topmost', False)
-
-    root.destroy()
-
-    return arquivo
-
+    # if uploaded_file is not None:
+    #     # Se um arquivo foi carregado, o pandas pode lê-lo diretamente
+    #     try:
+    #         df = pd.read_excel(uploaded_file)
+    #         st.write("Conteúdo do arquivo Excel:")
+    #         st.dataframe(df) # Exibe o DataFrame como uma tabela no Streamlit
+    #     except Exception as e:
+    #         st.error(f"Erro ao ler o arquivo Excel: {e}")
+    #         st.write("Certifique-se de que o arquivo é um arquivo Excel válido.")
+    # else:
+    #     st.write("Por favor, carregue um arquivo Excel para visualizar o conteúdo.")
+    
+    return uploaded_file
 #< ------------------------------------------------------------------------------------------------------------------------------
 
 click = st.button("Selecione um arquivo (.csv)", 
